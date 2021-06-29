c442b9341bdd381e7f48fa246aaeb8452438fd0a
vamandeshmukh <vamandeshmukh@gmail.com>
2021-06-29 18:24:25 +0530

    Commit
    COMMIT

--- src/main/java/com/cg/jpa/demo/App.java
+++ src/main/java/com/cg/jpa/demo/App.java
@@ -6,41 +6,50 @@ import javax.persistence.Persistence;
 
 /**
  * 
- * @author Vaman
+ * @author Vaman Deshmukh
  *
  */
+
+// ORM - concept 
+// JPA - specification 
+
+// 1. hibernate - tool / implementation 
+// 2. toplink - tool / implementation 
+// 3. ibatis - tool / implementation 
 
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnitName");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("abc");
 		EntityManager manager = emf.createEntityManager();
 		manager.getTransaction().begin();
 
-		// insert
-//		Employee emp2 = new Employee(52, "Ccc");
-//		manager.persist(emp2);
-//		System.out.println(manager.find(Employee.class, emp2.getEid()).toString());
-//		Employee emp3 = new Employee(55, "Zzz");
-//		manager.persist(emp3);
-//		System.out.println(manager.find(Employee.class, emp3.getEid()).toString());
-//		Employee emp4 = new Employee(55, "Zzz");
-//		manager.persist(emp4);
-//		System.out.println(manager.find(Employee.class, emp4.getEid()).toString());
+		// insert - persist()
+		Employee emp = new Employee(101, "Sonu");
+		manager.persist(emp);
+		System.out.println(manager.find(Employee.class, 101).toString());
+		Employee emp2 = new Employee(55, "Zzz");
+		manager.persist(emp2);
+		System.out.println(manager.find(Employee.class, emp2.getEid()).toString());
+		Employee emp3 = new Employee(56, "Zzz");
+		manager.persist(emp3);
+		System.out.println(manager.find(Employee.class, emp3.getEid()).toString());
 
-		// update
-//		emp4.setEname("Xxx");
-//		manager.merge(emp4);
-//		System.out.println(manager.find(Employee.class, emp4.getEid()).toString());
+		// update - merge()
+		Employee emp4 = new Employee(102, "Monu");
+		System.out.println(manager.find(Employee.class, emp4.getEid()).toString());
+		emp4.setEname("Tonu");
+		manager.merge(emp4);
+		System.out.println(manager.find(Employee.class, emp4.getEid()).toString());
 
-		// delete
-//		Employee emp5 = new Employee(56, "Ddd");
-//		manager.persist(emp5);
-//		System.out.println(manager.find(Employee.class, emp5.getEid()).toString());
-//		manager.remove(emp5);
-//		if (null == manager.find(Employee.class, emp5.getEid()))
-//			System.out.println("Removed");
+		// delete - remove()
+		Employee emp5 = new Employee(103, "Ponu");
+		manager.persist(emp5);
+		System.out.println(manager.find(Employee.class, emp5.getEid()).toString());
+		manager.remove(emp5);
+		if (null == manager.find(Employee.class, emp5.getEid()))
+			System.out.println("Removed");
 
 		manager.getTransaction().commit();
 		System.out.println("End");
